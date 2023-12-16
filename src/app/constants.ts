2f0ffacdda6f251561296620ed653f3ca4621eef
Agnita Paul <agnifiraz@gmail.com>
2023-12-16 11:53:04 -0500

    Updated Contants.ts for AWS (EC2) with Docker

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
-export const BASEURL: string = 'http://localhost:8080/api';
-export const PDFURL = 'http://localhost:8080/PurchaseOrderPDF?porderid=';
-//export const BASEURL = '/api';
-//export const PDFURL = '/PurchaseOrderPDF?porderid=';
+//export const BASEURL: string = 'http://localhost:8080/api';
+//export const PDFURL = 'http://localhost:8080/PurchaseOrderPDF?porderid=';
+export const BASEURL = '/api';
+export const PDFURL = '/PurchaseOrderPDF?porderid=';
